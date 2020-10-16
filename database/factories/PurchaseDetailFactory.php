b8a329153310d31f02bdf245abc885a8992af800
mounitbhandari <tataibhandari@gmail.com>
2020-10-16 21:24:52 +0530

    nothing added

---
+++ database/factories/PurchaseDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Purchase_detail;
+use Faker\Generator as Faker;
+
+$factory->define(Purchase_detail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
