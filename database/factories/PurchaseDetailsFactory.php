b15ff4f240f83e485dfb9e37564a8c8a9a2b01d6
ritajaghosh <ritajaghosh@gmail.com>
2020-10-16 22:37:27 +0530

    create model

---
+++ database/factories/PurchaseDetailsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\purchase_details;
+use Faker\Generator as Faker;
+
+$factory->define(purchase_details::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
